f9e75020c39239992fe2b85273bf5a1d08d2b3c4
Akash Gaur <akgaur.mits@gmail.com>
2025-09-26 21:44:04 +0530

    Updated Singly linked list practice problem

--- 11_Linked_List/03_Singly_linked_list_practice_problem.py
+++ 11_Linked_List/03_Singly_linked_list_practice_problem.py
@@ -1,71 +1,54 @@
-Write a Program to reverse the Linked List. (Both Iterative and recursive)
- 
-Reverse a Linked List in group of Given Size. [Very Imp]
- 
-Write a program to Detect loop in a linked list.
- 
-Write a program to Delete loop in a linked list.
- 
-Find the starting point of the loop. 
- 
-Remove Duplicates in a sorted Linked List.
- 
-Remove Duplicates in a Un-sorted Linked List.
- 
-Write a Program to Move the last element to Front in a Linked List.
- 
-Add “1” to a number represented as a Linked List.
- 
-Add two numbers represented by linked lists.
- 
-Intersection of two Sorted Linked List.
- 
-Intersection Point of two Linked Lists.
- 
-Merge Sort For Linked lists.[Very Important]
- 
-Quicksort for Linked Lists.[Very Important]
- 
-Find the middle Element of a linked list.
- 
-Check if a linked list is a circular linked list.
- 
-Split a Circular linked list into two halves.
- 
-Write a Program to check whether the Singly Linked list is a palindrome or not.
- 	
-Deletion from a Circular Linked List.
- 	
-Reverse a Doubly Linked list.
- 	
-Find pairs with a given sum in a DLL.
- 	
-Count triplets in a sorted DLL whose sum is equal to given value “X”.
- 
-Sort a “k”sorted Doubly Linked list.[Very IMP]
- 
-Rotate DoublyLinked list by N nodes.
-	
-Rotate a Doubly Linked list in group of Given Size.[Very IMP]
-	
-Can we reverse a linked list in less than O(n) ?
-	
-Why Quicksort is preferred for. Arrays and Merge Sort for LinkedLists ?
-	
-Flatten a Linked List
-	
-Sort a LL of 0's, 1's and 2's
-	
-Clone a linked list with next and random pointer
+# Linked List Practice Problems
 
-Merge K sorted Linked list
+## Singly Linked List Problems
 
-Multiply 2 no. represented by LL
+1.  Write a Program to reverse the Linked List. (Both Iterative and
+    Recursive)
+2.  Reverse a Linked List in group of Given Size. **\[Very Important\]**
+3.  Write a program to Detect loop in a linked list.
+4.  Write a program to Delete loop in a linked list.
+5.  Find the starting point of the loop.
+6.  Remove Duplicates in a sorted Linked List.
+7.  Remove Duplicates in an Un-sorted Linked List.
+8.  Write a Program to Move the last element to Front in a Linked List.
+9.  Add "1" to a number represented as a Linked List.
+10. Add two numbers represented by linked lists.
+11. Intersection of two Sorted Linked List.
+12. Intersection Point of two Linked Lists.
+13. Merge Sort For Linked lists. **\[Very Important\]**
+14. Quicksort for Linked Lists. **\[Very Important\]**
+15. Find the middle Element of a linked list.
+16. Check if a linked list is a circular linked list.
+17. Split a Circular linked list into two halves.
+18. Write a Program to check whether the Singly Linked list is a
+    palindrome or not.
+19. Deletion from a Circular Linked List.
 
-Delete nodes which have a greater value on right side
+## Doubly Linked List Problems
 
-Segregate even and odd nodes in a Linked List
+20. Reverse a Doubly Linked list.
+21. Find pairs with a given sum in a DLL.
+22. Count triplets in a sorted DLL whose sum is equal to given value
+    "X".
+23. Sort a "k" sorted Doubly Linked list. **\[Very Important\]**
+24. Rotate Doubly Linked list by N nodes.
+25. Rotate a Doubly Linked list in group of Given Size. **\[Very
+    Important\]**
 
-Program for n’th node from the end of a Linked List
+## Conceptual / Theoretical Questions
 
-Find the first non-repeating character from a stream of characters
+26. Can we reverse a linked list in less than O(n)?
+27. Why Quicksort is preferred for Arrays and Merge Sort for Linked
+    Lists?
+
+## Advanced Linked List Problems
+
+28. Flatten a Linked List.
+29. Sort a Linked List of 0's, 1's and 2's.
+30. Clone a linked list with next and random pointer.
+31. Merge K sorted Linked lists.
+32. Multiply 2 numbers represented by Linked Lists.
+33. Delete nodes which have a greater value on the right side.
+34. Segregate even and odd nodes in a Linked List.
+35. Program for n'th node from the end of a Linked List.
+36. Find the first non-repeating character from a stream of characters.
