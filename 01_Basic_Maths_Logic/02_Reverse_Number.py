05a4a9f0d7f606b56866dcf1afb5ece86cb5cd82
Akash Gaur <akgaur.mits@gmail.com>
2026-04-21 22:34:06 +0530

    feat(Reverse_Number): Added recursive and string slicing approaches to reverse a number

--- 01_Basic_Maths_Logic/02_Reverse_Number.py
+++ 01_Basic_Maths_Logic/02_Reverse_Number.py
@@ -15,6 +15,7 @@ Time Complexity:
   where `n` is the input number.
 """
 
+# Approach 1: Using arithmetic operations
 def reverseNumber(num):
     if num <= 9:
         return n
@@ -27,14 +28,31 @@ def reverseNumber(num):
         num //= 10                       # Remove the last digit
 
     return rev_num
-        
+
+# Approach 2: Using recursion
+def reverseNumber_Recursive(num):
+    """Reverses a number using recursion."""
+    if num <= 9:
+        return num
+    
+    return reverseNumber_Recursive(num % 10) * (10 ** (len(str(num)) - 1)) + reverseNumber_Recursive(num // 10)
+
+
+# Approach 3: Using string slicing
+def reverseNumber_String(num):
+    """Reverses a number using string slicing."""
+    return int(str(num)[::-1])
+
+
 
 # --------- Main Program ---------
 try:
     Test_Case = int(input("Enter Number of Test Case: "))
     while Test_Case:
         n = int(input("Enter a number: "))
-        print("Reversed number:", reverseNumber(n))
+        print("Reversed number (Arithmetic): ", reverseNumber(n))
+        print("Reversed number (Recursive): ", reverseNumber_Recursive(n))
+        print("Reversed number (String): ", reverseNumber_String(n))
         Test_Case -= 1
 except ValueError:
     print("Invalid input! Please enter an integer.")
